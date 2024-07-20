55a8cd9ab2e5ed305059d81cb605f047e0a974fd
TarcisioLuiz <tarcisioluizfilho@gmail.com>
2024-07-20 01:52:40 -0300

    Implementação do componente header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    //template: '<h1>Hello World</h1>',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
+    //styles: ['h1 { color: red}']
+})
+export class HeaderComponent {}
